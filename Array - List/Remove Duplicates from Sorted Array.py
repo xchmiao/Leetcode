873a1b05a6abd67844f74d40009f4e20579e6ef3
xchmiao <xchmiao@gmail.com>
2017-11-14 16:34:09 -0800

    Update Remove Duplicates from Sorted Array.py

--- Array - List/Remove Duplicates from Sorted Array.py
+++ Array - List/Remove Duplicates from Sorted Array.py
@@ -4,11 +4,18 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: int
+        
+        Since the array is already sorted, we can keep two pointers i and k, where ii is the slow-runner while jj is the fast-runner. 
+        As long as nums[i] = nums[k], we increment k to skip the duplicate; 
+        else the duplicate run has ended, so increase i by 1 and then copy its value to nums[i]
+
         """
         if len(nums) <= 1:
             return len(nums)
         else:
+            i = 0
             for k in range(1, len(nums)):
-                if nums[k] != num[i]:
-                
-                    
+                if nums[k] != nums[i]:
+                    i = i+1
+                    nums[i] = nums[k]
+            return i+1
