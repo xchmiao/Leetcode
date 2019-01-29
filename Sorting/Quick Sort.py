353b0bed581b1c8e1aed8cd92caf90522eeb6e55
xchmiao <xchmiao@gmail.com>
2019-01-29 11:12:51 -0800

    Create Quick Sort.py

---
+++ Sorting/Quick Sort.py
@@ -0,0 +1,29 @@
+class Solution:
+    # @param {int[]} A an integer array
+    # @return nothing
+    def sortIntegers2(self, A):
+        # Write your code here
+        self.quick_sort(A, 0, len(A) - 1)
+
+    def quick_sort(self, A, start, end):
+    	if start >= end:
+    		return
+
+    	left, right = start, end
+    	pivot = A[(start + end) // 2]
+
+    	while left <= right:
+    		while left <= right and A[left] < pivot:
+    			left += 1
+    		while left <= right and A[right] > pivot:
+    			right += 1
+
+    		if left <= right:
+    			A[left], A[right] = A[right], A[left]
+
+    			left += 1
+    			right += 1
+    			
+    	# When the while loop stops, right = left - 1
+    	quick_sort(self, A, start, right)
+    	quick_sort(self, A, left, end)
