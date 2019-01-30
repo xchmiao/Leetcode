467e29e43581f28a9f2c22586525705d785563be
xchmiao <xchmiao@gmail.com>
2019-01-30 14:41:01 -0800

    Create Find K Closest Elements  in BST.py

---
+++ DFS/Binary Tree Based DFS/Find K Closest Elements  in BST.py
@@ -0,0 +1,82 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the given BST
+    @param target: the given target
+    @param k: the given k
+    @return: k values in the BST that are closest to the target
+    """
+    def closestKValues(self, root, target, k):
+        # write your code here
+        if not root or k == 0:
+            return []
+
+        lower_stack = self.get_stack(root, target)
+        upper_stack = list(lower_stack)
+
+        if lower_stack[-1].val > target:
+            self.move_lower(root, target)
+        else:
+            self.move_upper(root, target)
+
+        result = []
+
+        for i in range(k):
+            if self.is_lower_closer(lower_stack, upper_stack, target):
+                result.append(lower_stack[-1].val)
+                self.move_lower(lower_stack)
+            else:
+                result.append(upper_stack[-1].val)
+                self.move_upper(upper_stack)
+
+        return result
+
+
+    def get_stack(self, root, target):
+        stack = []
+        while root:
+            stack.append(root)
+            if target < root.val:
+                root = root.left
+            else:
+                root = root.right
+
+    def move_upper(self, stack):
+        if stack[-1].right:
+            node = stack[-1].right
+            while node:
+                stack.append(node)
+                node = node.left
+        else:
+            node = stack.pop()
+            while stack and stack[-1].right == node:
+                node = stack.pop()
+
+
+    def move_lower(self, stack):
+        if stack[-1].left:
+            node = stack[-1].left
+            while node:
+                stack.append(node)
+                node = node.right
+        else:
+            node = stack.pop()
+            while stack and stack[-1].left == node:
+                node = stack.pop()
+
+
+    def is_lower_closer(self, lower_stack, upper_stack, target):
+        if not upper_stack:
+            return True
+
+        if not lower_stack:
+            return False
+
+        return target - lower_stack[-1].val < upper_stack[-1].val - target
