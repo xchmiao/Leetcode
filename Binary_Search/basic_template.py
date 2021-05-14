49665f29472fa0dec8d8a689886ce497c803b2ce
xchmiao <xchmiao@gmail.com>
2021-05-13 22:24:17 -0700

    Update basic_template.py

--- Binary_Search/basic_template.py
+++ Binary_Search/basic_template.py
@@ -1,44 +1,21 @@
-## Given a sorted array nums, and a target number,
-## if target is in nums, return it's index, otherwise return -1
-
-## Interative solution
-
 class Solution:
-	def binarySearch(self, nums, target):
-		if len(nums) == 0:
-			return -1
-
-		start, end = 0, len(nums) - 1
-
-        # start <= end ensure to find the target item is the last element
-		while (start <= end):
-			mid = (start + end)/2
-			if nums[mid] == target:
-				return mid
-			if nums[mid] < target:
-				start = mid + 1
-			else:
-				end = mid - 1
-
-		return -1
-
-
-## Recursive solution
-
-class Solution:
-	def binarySearch(self, nums, target):
-		# if len(nums) == 0:
-		# 	return -1
-		start, end = 0, len(nums) - 1
-		return self._findTarget(nums, start, end, target)
-
-	def _findTarget(self, nums, start, end, target):
-		if start > end:
-			return -1
-
-		mid = (start + end)/2
-		if nums[mid] == target:
-			return mid
-		if nums[mid] < target:
-			return self._findTarget(nums, mid + 1, end, target)
-		return self._findTarget(nums, start, mid - 1, target)
+    # @param nums: The integer array
+    # @param target: Target number to find
+    # @return the first position of target in nums, position start from 0 
+    def binarySearch(self, nums, target):
+        if len(nums) == 0:
+            return -1
+            
+        start, end = 0, len(nums) - 1
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if nums[mid] < target:
+                start = mid
+            else:
+                end = mid
+                
+        if nums[start] == target:
+            return start
+        if nums[end] == target:
+            return end
+        return -1
